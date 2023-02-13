9189d352b12037da797e41190a6430df17291317
Matt Holmstrom <mjholmstr@gmail.com>
2023-02-13 14:56:22 -0600

    Add buildTree method and prettyPrint function to visualize

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -1,3 +1,15 @@
+// Function to print BST to terminal
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node.right !== null) {
+         prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+}
+
+
 class Node {
     constructor(data = null, left = null, right = null) {
         this.data = data;
@@ -9,11 +21,28 @@ class Node {
 class Tree {
     constructor(array) {
         this.root = this.buildTree(array);
+        prettyPrint(this.root);
     }
-}
 
-function removeDuplicatesAndSort(arr) {
+    removeDuplicatesAndSort(arr) {
     return [...new Set(arr)].sort((a, b) => a - b);
+    }
+
+
+    buildTree(arr) {
+        let filteredArray = this.removeDuplicatesAndSort(arr);
+        if (filteredArray.length === 0) return null;
+        const midpoint = parseInt(filteredArray.length / 2);
+        const root = new Node(
+            filteredArray[midpoint],
+            this.buildTree(filteredArray.slice(0, midpoint)),
+            this.buildTree(filteredArray.slice(midpoint + 1))
+        );
+        return root;
+    }
+
+    
 }
 
-console.log(removeDuplicatesAndSort([2, 5, 34, 2, 6, 3, 14, 3]))
+let testInputArray = [1, 2, 9, 3, 4, 2, 5, 6, 7];
+balancedBST = new Tree (testInputArray);
